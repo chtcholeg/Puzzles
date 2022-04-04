5d26296a89bce72a0baf7b9674ea6ecb6a230493
Oleg Shchepilov <oleg.shchepilov@mapbox.com>
2022-04-04 20:16:02 +0300

    Currency exchange: - Added description - Changed display information

--- сurrency exchange/Solution_on_swift.playground/Contents.swift
+++ сurrency exchange/Solution_on_swift.playground/Contents.swift
@@ -1,3 +1,18 @@
+// Approach
+//  We will build chains - a chain for each currency (except the main). Step by step:
+//  1. Each chain (for currency #i) looks like: X_1 -> X_i
+//  2. For each X_i, among the built chains, we find the one that is the best when added X_i at the end of the chain.
+//  3. Checking if there are any improvements. + Maybe, there is a chain that satisfies the condition if we add X_1 to the end of this chain
+//  4. Go to 2.
+//
+// Algorithm complexity
+//  O(N^2 * K), where N = currency count, K = chain length
+//  It's not a good complexity but I haven't found better
+//
+// To simplify, in this implementation, X_1 is the last one in the list of currencies
+//
+                                    
+
 let currencyCount = 10
 let intermediateCurrencyCount = currencyCount - 1
 let targetCurrencyIndex = currencyCount - 1
@@ -17,11 +32,9 @@ struct Chain {
         return copy
     }
     
-    func Dump() -> String {
-        var result = indices.description
-        result.append(", conv: ")
-        result.append(String(convertResult))
-        result.append(", target: ")
+    func Dump(_ addMainCurrency : Bool) -> String {
+        var result = chainToString(addMainCurrency)
+        result.append(", result: ")
         result.append(String(loopResult))
         return result
     }
@@ -34,12 +47,12 @@ struct Chain {
         } else {
             let prevIndex = indices.last!
             indices.append(newIndex)
-            convertResult = ConvertCurrency(convertResult, prevIndex, newIndex)
-            loopResult = ConvertCurrency(convertResult, newIndex, targetCurrencyIndex)
+            convertResult = convertCurrency(convertResult, prevIndex, newIndex)
+            loopResult = convertCurrency(convertResult, newIndex, targetCurrencyIndex)
         }
     }
     
-    func ConvertCurrency(_ currencyValueFrom: Double, _ currencyIndexFrom: Int, _ currencyIndexTo: Int) -> Double
+    private func convertCurrency(_ currencyValueFrom: Double, _ currencyIndexFrom: Int, _ currencyIndexTo: Int) -> Double
     {
         if currencyIndexFrom == currencyIndexTo {
             return currencyValueFrom
@@ -51,6 +64,20 @@ struct Chain {
             koef = 1.1
         }
         return koef * currencyValueFrom
+    }
+    
+    private func chainToString(_ addMainCurrency : Bool) -> String
+    {
+        var result = ""
+        for index in indices {
+            result.append(result.isEmpty ? "" : " -> ")
+            result.append(String(index))
+        }
+        if addMainCurrency {
+            result.append(result.isEmpty ? "" : " -> ")
+            result.append(String(targetCurrencyIndex))
+        }
+        return result
     }
 }
 
@@ -105,5 +132,6 @@ var chain = findChain()
 if chain == nil {
     print("NO")
 } else {
-    print(chain!.Dump())
+    print("Target currency: " + String(targetCurrencyIndex))
+    print(chain!.Dump(true))
 }
